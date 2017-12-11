374634315b12d6e1f2673ff29f92c3e91ece2c91
Dan Aschenbach <phily_phreak@yahoo.com>
2017-12-11 15:59:11 -0600

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -8,13 +8,12 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
-class Username(Base):
-    __tablename__ = 'username'
+class User(Base):
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    picture = Column(String(250))
 
     @property
     def serialize(self):
@@ -30,8 +29,8 @@ class Category(Base):
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('username.id'))
-    user = relationship(Username)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -47,11 +46,11 @@ class Item(Base):
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
-    price = Column(String(8))
-    category_id = Column(Integer,ForeignKey('category.id'))
+    price = Column(String(10))
+    category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-    user_id = Column(Integer, ForeignKey('username.id'))
-    user = relationship(Username)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
